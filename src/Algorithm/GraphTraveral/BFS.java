aaca1277775d77f8114930a4f6db58626b34caca
박정규 <srk99279@naver.com>
2022-11-23 15:48:42 +0900

    feat : 파일 읽어들여오기
    차후 연결리스트와 하기 전에 알파벳을 아스키코드화 하여 계산하는 것이 나아보임.

--- src/Algorithm/GraphTraveral/BFS.java
+++ src/Algorithm/GraphTraveral/BFS.java
@@ -3,23 +3,38 @@ package Algorithm.GraphTraveral;
 import java.util.*;
 import java.io.*;
 
+// ASCII 코드로 변형해서 LinkedList로 이용하자...
+
 public class BFS {
+    static Map<String, String[]> graph = new HashMap<>();
+
+    public static void BFS(boolean[] visited) {
+        Queue<String> q = new LinkedList<String>();
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("Graph1.txt"));
         ArrayList<String[]> graph_arr = new ArrayList<>();
-        LinkedList<String> graph = new LinkedList<>();
 
         String str;
         while ((str = br.readLine()) != null) {
             String[] s = str.split(" ");
             graph_arr.add(s);
         }
-        boolean [] visited = new boolean[graph_arr.size()];
 
-        for (String[] c : graph_arr){
-            System.out.println(Arrays.toString(c));
+        for (String[] c : graph_arr) {
+            String[] v = new String[(int) c.length / 2];
+            for (int i = 0; i < v.length; i++) {
+                v[i] = c[i * 2 + 1];
+            }
+            graph.put(c[0], v);
         }
 
+        boolean[] visited = new boolean[graph_arr.size()];
+
+        graph.entrySet().forEach(item -> System.out.println(item.getKey() + " : " + Arrays.toString(item.getValue())));
+        System.out.println(Arrays.toString(visited));
+
+        BFS(visited);
     }
 }
