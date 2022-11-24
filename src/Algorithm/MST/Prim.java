a8bd182ecf05444d4767f7550ffde6e823e3c1dc
박정규 <srk99279@naver.com>
2022-11-24 19:38:33 +0900

    feat : MST-Prim 알고리즘 구현
    연결리스트를 사용한 풀이.

--- src/Algorithm/MST/Prim.java
+++ src/Algorithm/MST/Prim.java
@@ -1,4 +1,108 @@
 package Algorithm.MST;
 
+import com.sun.javafx.scene.traversal.Algorithm;
+
+import javax.swing.event.InternalFrameAdapter;
+import java.io.*;
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int start;
+    int end;
+    int cost;
+
+    Node(int start, int end, int cost) {
+        this.start = start;
+        this.end = end;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node node) {
+        return node.cost >= this.cost ? -1 : 1;
+    }
+
+    public String toString() {
+        return String.valueOf(this.start + " " + this.end + " " + this.cost + "\n");
+    }
+}
+
 public class Prim {
+    public static ArrayList<Node>[] nodeList;
+    public static ArrayList<Node> graph = new ArrayList<Node>();
+    public static boolean[] visited;
+    static StringBuilder sb = new StringBuilder("");
+    static int cost = 0;
+
+    public static void search() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("Graph1.txt"));
+        ArrayList<String[]> vertex = new ArrayList<>();
+
+        String str;
+        while ((str = br.readLine()) != null) {
+            String[] s = str.split(" ");
+            vertex.add(s);
+        }
+
+        for (String[] c : vertex) {
+            for (int i = 1; i < c.length; i += 2) {
+                Integer[] v = new Integer[3];
+                int input_start = c[0].charAt(0) - 65;
+                int input_end = c[i].charAt(0) - 65;
+                int input_cost = Integer.parseInt(c[i + 1]);
+                graph.add(new Node(input_start, input_end, input_cost));
+            }
+        }
+
+        nodeList = new ArrayList[graph.size()];
+        for (int i = 0; i < graph.size(); i++) {
+            nodeList[i] = new ArrayList<Node>();
+        }
+
+        for (int i = 0; i < graph.size(); i++) {
+            int start = graph.get(i).start;
+            int end = graph.get(i).end;
+            int cost = graph.get(i).cost;
+            nodeList[start].add(new Node(start, end, cost));
+            nodeList[end].add(new Node(end, start, cost));
+        }
+
+        visited = new boolean[graph.size()];
+        Arrays.fill(visited, false);
+        prim(0); // 추후 시작 지점 입력받기. 현재는 A로 시작한 경우임
+//        System.out.println(graph.toString());
+
+        System.out.println(cost);
+        System.out.println(sb);
+    }
+
+    public static void prim(int p) {
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        Queue<Integer> q = new LinkedList<>();
+
+        q.offer(p);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            visited[node] = true;
+            for (Node n : nodeList[node]) {
+                if (!visited[n.end]) {
+                    pq.add(n);
+                }
+            }
+            while (!pq.isEmpty()) {
+                Node e = pq.poll();
+                if (!visited[e.end]) {
+                    q.add(e.end);
+                    visited[e.end] = true;
+                    sb.append(String.valueOf((char)(e.end + 65)));
+                    // StringBuilder 사용 각 나옴
+                    cost += e.cost;
+                    break;
+                }
+            }
+        }
+
+
+    }
 }
