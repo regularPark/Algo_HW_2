cd4b141d665b6e6e0cc4eceaae8978044887bf34
박정규 <srk99279@naver.com>
2022-11-24 14:45:09 +0900

    feat : DFS 구현
    DFS 구현 완료 및 Main에서 출력 완료.

--- src/Algorithm/GraphTraveral/DFS.java
+++ src/Algorithm/GraphTraveral/DFS.java
@@ -1,4 +1,42 @@
 package Algorithm.GraphTraveral;
 
+import java.io.*;
+import java.util.*;
+
 public class DFS {
+    static Map<Integer, Integer[]> graph = new HashMap<>();
+    public static void dfs(int v, boolean[] visited){
+        visited[v] = true;
+        System.out.print((char) (v + 65) + " ");
+
+        for (int i : graph.get(v)) {
+            if(!visited[i]) {
+                dfs(i, visited);
+            }
+        }
+    }
+    public static void search() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("Graph1.txt"));
+        ArrayList<String[]> graph_arr = new ArrayList<>();
+
+        String str;
+        while ((str = br.readLine()) != null) {
+            String[] s = str.split(" ");
+            graph_arr.add(s);
+        }
+
+        for (String[] c : graph_arr) {
+            Integer[] v = new Integer[(int) c.length / 2];
+            for (int i = 0; i < v.length; i++) {
+                v[i] = c[i * 2 + 1].charAt(0) - 65;
+            }
+            graph.put((c[0].charAt(0) - 65), v);
+        }
+
+        boolean[] visited = new boolean[graph_arr.size()];
+
+        System.out.print("DFS : ");
+        dfs(0, visited); // 차후에 v는 입력 받은 것으로 바꿔야 함
+        System.out.println();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import Algorithm.GraphTraveral.*;
+
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BFS bfs = new BFS();
+        bfs.search();
+
+        DFS dfs = new DFS();
+        dfs.search();
+
+    }
 }
